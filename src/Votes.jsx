7063ead1846c350cebf66a2fd1424cdb889c0adb
Jamie <93950259+jamiesp89@users.noreply.github.com>
2022-09-27 01:48:19 +0100

    Add colour change to icons when articles are voted up or down

--- src/Votes.jsx
+++ src/Votes.jsx
@@ -7,6 +7,8 @@ export default function Votes({ article }) {
   const [score, setScore] = useState(0);
   const [totalVotes, setTotalVotes] = useState(article.votes);
   let inc_vote = 0;
+  let upVote = "";
+  let downVote = "";
 
   const handleVote = (vote) => {
     if (vote === score) {
@@ -19,21 +21,33 @@ export default function Votes({ article }) {
       setScore(score + vote);
       inc_vote = vote;
     }
+
     patchArticle(article.article_id, inc_vote).then((article) => {
       console.log(article.votes);
       setTotalVotes(article.votes);
     });
   };
 
+  if (score === 1) {
+    upVote = "success";
+  } else if (score === -1) {
+    downVote = "success";
+  } else {
+    upVote = "";
+    downVote = "";
+  }
+
   return (
     <div>
       <ArrowUpwardIcon
+        color={upVote}
         onClick={() => {
           handleVote(1);
         }}
       ></ArrowUpwardIcon>
       <p>{totalVotes}</p>
       <ArrowDownwardIcon
+        color={downVote}
         onClick={() => {
           handleVote(-1);
         }}
