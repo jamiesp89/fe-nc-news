ef2bf4d2546f040e0c0371438a4649bd6eab2670
Jamie <93950259+jamiesp89@users.noreply.github.com>
2022-12-21 15:17:14 +0000

    add comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+// import React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// import the index.css file
 import "./index.css";
+
+// import the App component
 import App from "./App";
 
+// Create a root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App component inside the root element
 root.render(
   <React.StrictMode>
     <App />
